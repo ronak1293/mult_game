980c0711cdc931c584a19f6b0d20562bc2ceb987
Ronak <2022ucp1729@mnit.ac.in>
2025-07-03 09:34:19 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+
+const http=require('http')
+const server=http.createServer(app);
+const {Server}=require('socket.io')
+const io=new Server(server,{
+  pingInterval:2000,
+  pingTimeout:4000
+});
+
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +17,49 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+const backendPlayers={}
+io.on('connection',(socket)=>{
+  console.log('user connected');
+  backendPlayers[socket.id]={
+    x:500*Math.random(),
+    y:500*Math.random(),
+    color:`hsl(${360*Math.random()},100%,50%)`
+  }
+  io.emit('updatePlayers',backendPlayers)
+
+  socket.on('disconnect',(reason)=>{
+    console.log(reason);
+
+    delete backendPlayers[socket.id];
+    io.emit('updatePlayers',backendPlayers);
+    
+  })
+
+  socket.on('keydown',(keycode)=>{
+
+    switch (keycode){
+    case "KeyW":
+      backendPlayers[socket.id].y-=30;
+      break
+    case "KeyA":
+      backendPlayers[socket.id].x-=30;
+      break
+    case "KeyS":
+      backendPlayers[socket.id].y+=30;
+      break
+    case "KeyD":
+      backendPlayers[socket.id].x+=30;
+      break 
+    
+  }
+  })
+
+})
+
+setInterval(()=>{
+   io.emit('updatePlayers',backendPlayers)
+},15)
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
